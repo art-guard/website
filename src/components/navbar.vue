7b28acefd4c8137f11608f81dfa24519f3b643af
inazumaron <skuyanon@gmail.com>
2021-08-11 14:18:50 +0800

    added navbar

---
+++ src/components/navbar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="navbar">
+        <div class="navbarComp"> 
+            <b> Login </b> 
+        </div>
+        <div class="navbarComp">
+            Guard Your art
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+    body{
+        background-color: #1f2e35;
+    }
+    .navbar{
+    display: flex;
+    flex-direction: row-reverse;
+    text-align: right;
+    color: #40df9f;
+    }
+    .navbarComp{
+        margin-left: 1%;
+        margin-right: 1%;
+    }
+</style>
